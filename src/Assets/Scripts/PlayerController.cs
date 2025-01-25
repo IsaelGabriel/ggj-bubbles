b231570ae85362f4f454668e7b7872b55190cacc
IsaelGSF <isaelgsfaria@gmail.com>
2025-01-25 01:44:02 -0300

    Add player jump

--- src/Assets/Scripts/PlayerController.cs
+++ src/Assets/Scripts/PlayerController.cs
@@ -12,8 +12,17 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float _maxVelocity = 50f;
 
+    [SerializeField]
+    private float _jumpForce = 100f;
+
+    [SerializeField]
+    private Transform bottom;
+
 
     private Rigidbody _rigidbody;
+
+    private bool _grounded = true;
+
 
     // Start is called before the first frame update
     void Start()
@@ -23,11 +32,13 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
-
+        Jump();
     }
     
     void FixedUpdate()
     {
+        CheckGrounded();
+
         Move();
     }
 
@@ -38,23 +49,32 @@ public class PlayerController : MonoBehaviour
         Vector3 currentVelocity = _rigidbody.velocity;
         currentVelocity.y = 0f;
 
+        if(input.magnitude != 0f && currentVelocity.magnitude <= _maxVelocity) {
+            currentVelocity += input * _acceleration * Time.deltaTime;
+            currentVelocity = Vector3.ClampMagnitude(currentVelocity, _maxVelocity);
+        } else {
+            currentVelocity = currentVelocity.normalized * Math.Clamp(currentVelocity.magnitude - (_deceleration * Time.deltaTime), 0f, currentVelocity.magnitude);
+            Vector3 newVelocity = currentVelocity + input * _acceleration * Time.deltaTime;
+            if(Math.Abs(newVelocity.x) < Math.Abs(currentVelocity.x)) currentVelocity.x = newVelocity.x;
+            if(Math.Abs(newVelocity.y) < Math.Abs(currentVelocity.y)) currentVelocity.y = newVelocity.y;
 
-        currentVelocity = currentVelocity.normalized * Math.Clamp(currentVelocity.magnitude - (_deceleration * Time.deltaTime), 0f, currentVelocity.magnitude);
-
-
-        if(input.magnitude != 0f) {
-            if(currentVelocity.magnitude <= _maxVelocity) {
-                currentVelocity += input * _acceleration * Time.deltaTime;
-                currentVelocity = Vector3.ClampMagnitude(currentVelocity, _maxVelocity);
-            }else {
-                Vector3 newVelocity = currentVelocity + input * _acceleration * Time.deltaTime;
-                if(Math.Abs(newVelocity.x) < Math.Abs(currentVelocity.x)) currentVelocity.x = newVelocity.x;
-                if(Math.Abs(newVelocity.y) < Math.Abs(currentVelocity.y)) currentVelocity.y = newVelocity.y;
-            }
         }
 
         currentVelocity.y = _rigidbody.velocity.y;
         _rigidbody.velocity = currentVelocity;
 
+    }
+    private void Jump() {
+        if(Input.GetButtonDown("Jump") && _grounded) {
+            _rigidbody.AddForce(Vector3.up * _jumpForce);
+        }
+    }
+
+    private void CheckGrounded() {
+        RaycastHit hit;
+
+        if(Physics.Raycast(bottom.position, Vector3.down, out hit)) {
+            _grounded = hit.distance <= 0.05f;
+        }
     }
 }
