f85a8eeadca1f03f507395da3089ed55916ee1c4
IsaelGSF <isaelgsfaria@gmail.com>
2025-01-25 02:26:36 -0300

    Add base bubble

---
+++ src/Assets/Scripts/Bubble.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubble : MonoBehaviour
+{
+    public float force = 500f;
+
+    void OnCollisionEnter(Collision collision) {
+        if(collision.gameObject.GetComponent<Rigidbody>() != null) {
+            Vector3 contact = collision.GetContact(0).point;
+            Vector3 forceDirection = contact - transform.position;
+            
+            collision.gameObject.GetComponent<Rigidbody>().AddForceAtPosition(force * forceDirection, contact, ForceMode.Impulse);
+            Destroy(gameObject);
+        }
+    }
+}
