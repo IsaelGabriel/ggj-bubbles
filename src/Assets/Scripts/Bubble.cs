10992f8eb7c9b84fb68caba43c9e59989ea4be00
IsaelGabriel <isaelgsfaria@gmail.com>
2025-01-26 16:22:03 -0300

    Add audio files and audio players

--- src/Assets/Scripts/Bubble.cs
+++ src/Assets/Scripts/Bubble.cs
@@ -2,16 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bubble : MonoBehaviour
+public class Bubble : AudioSource
 {
     public float force = 500f;
 
     void OnCollisionEnter(Collision collision) {
+        Play();
         if(collision.gameObject.GetComponent<Rigidbody>() != null) {
             Vector3 contact = collision.GetContact(0).point;
             Vector3 forceDirection = contact - transform.position;
             
-            collision.gameObject.GetComponent<Rigidbody>().AddForceAtPosition(force * forceDirection, contact, ForceMode.Impulse);
+         1   collision.gameObject.GetComponent<Rigidbody>().AddForceAtPosition(force * forceDirection, contact, ForceMode.Impulse);
         }
         Destroy(gameObject);
     }
